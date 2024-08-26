f7cb1fcc6a8cc354541e8ca36b64f0292af11b25
3damp <amp206@gmail.com>
2024-08-26 19:46:55 +0100

    manifest+sw

---
+++ sw.js
@@ -0,0 +1,10 @@
+var GHPATH = '/pwa-tests-1';
+var APP_PREFIX = 'pwat1_';
+var VERSION = 'v_00';
+
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/styles.css`,
+  `${GHPATH}/script.js`
+]
